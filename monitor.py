cdaee3d101ab8ba0668af7a4329c2fcf09771c0e
Cooper Garren <cgarren18@icloud.com>
2022-12-17 19:23:07 -0500

    added indefinite running and lots of comments

--- monitor.py
+++ monitor.py
@@ -1,17 +1,57 @@
+# imports
+from datetime import datetime, timedelta
+import time
 import requests
 from bs4 import BeautifulSoup
 
-# URL = "https://www.bestbuy.com/site/bose-quietcomfort-45-wireless-noise-cancelling-over-the-ear-headphones-triple-black/6471291.p?skuId=6471291"
+# set the url
 URL = "https://www.bestbuy.com/site/sony-wh-1000xm4-wireless-noise-cancelling-over-the-ear-headphones-black/6408356.p?skuId=6408356"
-headers = {
-    "User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
-page = requests.get(URL, headers=headers)
-soup = BeautifulSoup(page.content, "html.parser")
-price = soup.find("div", {"class": "priceView-customer-price"})
-for tag in price.contents:
-    if 'class' in tag.attrs and "sr-only" in tag['class']:
-        continue
-    print(tag.get_text())
 
-# with open("bestbuy.html", "w") as file:
-#     file.write(str(soup))
+# set the fetching interval in seconds
+INTERVAL = 10
+
+
+def extractPageData(url):  # get beutiful soup data from url
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"
+    }
+    page = requests.get(url, headers=headers)
+    return BeautifulSoup(page.content, "html.parser")
+
+
+def checkPriceBestBuy(url):  # extract price for Best Buy page contents
+    soup = extractPageData(url)
+    price_div = soup.find("div", {"class": "priceView-customer-price"})
+    price = ""
+    for tag in price_div.contents:
+        if 'class' in tag.attrs and "sr-only" in tag['class']:
+            continue
+        price = tag.get_text()
+    return price
+
+
+def writeSoupToFile(soup):  # write page contents to file for data extraction or debugging
+    with open("soup.html", "w") as file:
+        file.write(str(soup))
+
+
+def main():  # kickoff code
+    while True:
+        # log the starting time
+        past_time = datetime.now()
+
+        ### BUSINESS LOGIC HERE ###
+        print("Updated Price:", checkPriceBestBuy(URL))
+
+        # sleep for the interval
+        time_delta = datetime.now() - past_time
+        if time_delta < timedelta(seconds=INTERVAL):
+            time.sleep(INTERVAL)
+        else:
+            sleep_time = (
+                time_delta - timedelta(seconds=INTERVAL)).total_seconds()
+            time.sleep(sleep_time)
+
+
+if __name__ == "__main__":
+    main()
